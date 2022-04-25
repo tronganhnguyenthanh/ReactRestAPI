e35edd63a598a0b351bdeee332693e9427e3b108
tronganhnguyenthanh <30815708+tronganhnguyenthanh@users.noreply.github.com>
2022-04-25 11:56:26 +0700

    [UPDATE][FE] build simple layout chat message with restful api

---
+++ client/src/app/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ChatMessage from "../components/ChatMessage";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChatMessage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
